ae94841efb758db4b2966e4352f4010bad4fcbfd
tonipcv <xppsalvador@gmail.com>
2025-02-06 18:27:54 +0000

    dsad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,16 +32,9 @@ export const metadata: Metadata = {
     telephone: false,
   },
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ]
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
   openGraph: {
     type: 'website',
@@ -97,14 +90,13 @@ export default function RootLayout({
   return (
     <html lang="en-US">
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
-        <link rel="alternate" hrefLang="en-US" href="https://koraxlabs.com" />
-        <meta httpEquiv="content-language" content="en-US" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-config" content="none" />
       </head>
       <body className={montserrat.className}>
         {children}
